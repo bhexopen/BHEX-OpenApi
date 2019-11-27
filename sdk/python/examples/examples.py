715b81ce88df40e7c27be5fd4359dad2630787bf
tangfulin <fulin.tang@bhex.io>
2019-11-27 16:20:58 +0800

    update entry point comment

--- sdk/python/examples/examples.py
+++ sdk/python/examples/examples.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         "https": "",
     }
 
-    entry_point = ''  # input your broker api entry point
+    entry_point = ''  # like: https://api.xxx.yyy/openapi/ where xxx.yyy is your base domain
     b = BrokerClient(entry_point, api_key='', secret='', proxies=proxies)
 
     print(b.time())
