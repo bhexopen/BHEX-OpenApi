981b9e67bc1c5a10263aad12ab33294dbd796813
tangfulin <fulin.tang@bhex.io>
2019-12-13 19:30:25 +0800

    update example

--- sdk/java/src/test/java/io/broker/api/test/constant/Constants.java
+++ sdk/java/src/test/java/io/broker/api/test/constant/Constants.java
@@ -2,13 +2,22 @@ package io.broker.api.test.constant;
 
 public class Constants {
 
-    // FIXME: change to a valid key and secret
+    // FIXME: change to a valid key and secret for test
     public final static String ACCESS_KEY = "";
     public final static String SECRET_KEY = "";
 
-    // FIXME: change to valid api urls
-    public static final String API_BASE_URL = "";
-    public static final String WS_API_BASE_URL = "";
-    public static final String WS_API_USER_URL = "";
+    // FIXME: please change to your valid api domain for test, for example: bhop.cloud
+    public static final String BASE_DOMAIN = "";
 
+    // REST api url format: https://api.BASE_DOMAIN
+    // for example: https://api.bhop.cloud
+    public static final String API_BASE_URL = "https://api." + BASE_DOMAIN;
+
+    // Websocket base api url format: wss://wsapi.BASE_DOMAIN/openapi/quote/ws/v1
+    // for example: wss://wsapi.bhop.cloud/openapi/quote/ws/v1
+    public static final String WS_API_BASE_URL =  "wss://wsapi." + BASE_DOMAIN +  "/openapi/quote/ws/v1";
+
+    // Websocket user api url format: wss://wsapi.BASE_DOMAIN/openapi/ws
+    // for example: wss://wsapi.bhop.cloud/openapi/ws
+    public static final String WS_API_USER_URL = "wss://wsapi." + BASE_DOMAIN + "/openapi/ws";
 }
